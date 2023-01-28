131f52f8fe02f1962d95f0a4e3f45ba5f588ca1c
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 23:02:55 -0500

    tipos_datos-py
    Creación de proyecto

---
+++ tipos_datos-py.py
@@ -0,0 +1,19 @@
+#Tipo de dato entero o largo
+numero = 15
+print(numero,type(numero))
+
+#Tipo de dato flotante
+numero_flotante = 15.5
+print(numero_flotante,type(numero_flotante))
+
+#Tipo de dato complejo
+numero_complejo = 15 + 8j
+print(numero_complejo,type(numero_complejo))
+
+#Tipo de dato string
+nombre = "Tu vas a volar"
+print(nombre,type(nombre))
+
+#Tipo de dato booleano
+verdadero_false = 3 == 3
+print(verdadero_false,type(verdadero_false))
